cd5771d0816f37edb52b3913139899b347e0806c
nikugr <54131805+nikugr@users.noreply.github.com>
2019-08-15 09:57:31 +0300

    Added everything before win check

--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var playerTurn = 1;
+
+const main = () => {
+  //let arr = generateArray(5, 5);
+  let board = document.getElementById("board");
+
+  board.appendChild(createTable(5, 5));
+};
+
+const turn = () => {
+  if(playerTurn === 1) {
+    playerTurn = 2;
+  } else {
+    playerTurn = 1;
+  }
+}
+
+const getMark = () => {
+  return playerTurn === 1 ? "X" : "O";
+}
+
+const checkForWinner = () => {
+  let board = document.getElementById("board");
+  let cells = board.getElementsByClassName("td");
+}
+
+/*
+const generateArray = (width, height) => {
+  let arr = [];
+  for (let y = 0; y < height; y++) {
+    arr[y] = [];
+    for (let x = 0; x < width; x++) {
+      arr[y][x] = 0;
+    }
+  }
+  return arr;
+};
+*/
+
+const createTable = (width, height) => {
+  let table = document.createElement("table");
+
+  for (let y = 0; y < height; y++) {
+    let row = document.createElement("tr");
+    table.appendChild(row);
+
+    for (let x = 0; x < width; x++) {
+      let cell = document.createElement("td");
+      let text = document.createTextNode("");
+      cell.appendChild(text);
+      cell.onclick = () => {handleCellClick(cell)};
+      row.appendChild(cell);
+    }
+  }
+
+  return table;
+};
+
+const handleCellClick = (elem) => {
+  // Legal move
+  if(elem.innerHTML === "") {
+    elem.innerHTML = getMark();
+    turn();
+  } 
+  // Illegal move
+  else {
+    console.log("Can't do that");
+  }
+}
+
+export default main;
