61bcec6b73947f62540710bcf735c3424c83610f
nikugr <54131805+nikugr@users.noreply.github.com>
2019-08-15 10:48:29 +0300

    First full version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import "./styles.css";
 
 var playerTurn = 1;
+var boardWidth = 5;
+var boardHeight = 5;
+var playing = true;
 
 const main = () => {
   //let arr = generateArray(5, 5);
   let board = document.getElementById("board");
 
-  board.appendChild(createTable(5, 5));
+  board.appendChild(createTable(boardWidth, boardHeight));
 };
 
 const turn = () => {
+  checkForWinner(getMark());
   if(playerTurn === 1) {
     playerTurn = 2;
   } else {
@@ -21,9 +25,72 @@ const getMark = () => {
   return playerTurn === 1 ? "X" : "O";
 }
 
-const checkForWinner = () => {
+const checkForWinner = (mark) => {
   let board = document.getElementById("board");
-  let cells = board.getElementsByClassName("td");
+  let cells = board.querySelectorAll("td");
+  
+  // Create array from table
+  let arr = [];
+  for (let y = 0; y < boardHeight; y++) {
+    arr[y] = [];
+    for (let x = 0; x < boardWidth; x++) {
+      arr[y][x] = cells[x + boardWidth * y].innerHTML;
+    }
+  }
+
+  // Check for the winner
+  for (let y = 0; y < boardHeight; y++) {
+    for (let x = 0; x < boardWidth; x++) {
+      if(mark !== arr[y][x]) {
+        continue;
+      }
+
+      // Check horizontal
+      if(x-2 >= 0 && x+2 < boardWidth) {
+        if(mark === arr[y][x-2] &&
+          mark === arr[y][x-1] &&
+          mark === arr[y][x+1] &&
+          mark === arr[y][x+2]) {
+            gameover();
+          }
+      }
+
+      // Check vertical
+      if(y-2 >= 0 && y+2 < boardHeight) {
+        if(mark === arr[y-2][x] &&
+          mark === arr[y-1][x] &&
+          mark === arr[y+1][x] &&
+          mark === arr[y+2][x]) {
+            gameover();
+          }
+      }
+
+      // Angles
+      if(x-2 >= 0 && x+2 < boardWidth && 
+        y-2 >= 0 && y+2 < boardHeight) {
+          // Check bottom-left to top-right
+          if(mark === arr[y-2][x+2] &&
+            mark === arr[y-1][x+1] &&
+            mark === arr[y+1][x-1] &&
+            mark === arr[y+2][x-2]) {
+              gameover();
+            }
+
+          // Check bottom-right to top-left
+          if(mark === arr[y-2][x-2] &&
+            mark === arr[y-1][x-1] &&
+            mark === arr[y+1][x+1] &&
+            mark === arr[y+2][x+2]) {
+              gameover();
+            }
+      }
+    }
+  }
+}
+
+const gameover = () => {
+  alert("Player " + playerTurn + " won!");
+  playing = false;
 }
 
 /*
@@ -60,7 +127,7 @@ const createTable = (width, height) => {
 
 const handleCellClick = (elem) => {
   // Legal move
-  if(elem.innerHTML === "") {
+  if(elem.innerHTML === "" && playing) {
     elem.innerHTML = getMark();
     turn();
   } 
